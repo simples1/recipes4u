4cfa70b4f8629ed8af9d5ef297ceb919727f2642
simples1 <abdullaaden@outlook.com>
2018-03-27 23:42:41 +0100

    change col name in ingredients table

--- db/migrate/20180327223909_create_ingredients.rb
+++ db/migrate/20180327223909_create_ingredients.rb
@@ -1,7 +1,8 @@
 class CreateIngredients < ActiveRecord::Migration[5.1]
   def change
     create_table :ingredients do |t|
-      t.string :ingredient
+      t.string :name
+      t.integer :recipe_id
 
       t.timestamps
     end
